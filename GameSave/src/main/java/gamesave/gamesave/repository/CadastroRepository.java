4353734b8c7dd274b55d278471c6dc1aa798f7ac
IagoCoelho <100175615+IagoCoelho@users.noreply.github.com>
2023-03-27 16:43:01 -0300

    Delete GameSave directory

--- GameSave/src/main/java/gamesave/gamesave/repository/CadastroRepository.java
+++
@@ -1,8 +0,0 @@
-package gamesave.gamesave.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import gamesave.gamesave.models.Cadastro;
-
-public interface CadastroRepository extends JpaRepository<Cadastro, Long>{
-    
-}
--- GameSave/src/main/java/gamesave/gamesave/repository/MetasRepository.java
+++
@@ -1,8 +0,0 @@
-package gamesave.gamesave.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import gamesave.gamesave.models.Metas;
-
-public interface MetasRepository extends JpaRepository<Metas, Long>{
-    
-}
